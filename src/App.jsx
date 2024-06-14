4460a02b4e6af5e5ab96ad233d73a1511c693cc8
d666o <d.666o@yandex.ru>
2024-06-14 15:59:53 +0300

    114

--- src/App.jsx
+++ src/App.jsx
@@ -4,31 +4,33 @@ import Operations from './Operations';
 import Result from './Result';
 
 const App = () => {
-  const [res, setRes] = useState('')
-  const [result, setResult] = useState('')
-  const resHandler = (item) => {
-    setRes(res + item)
-  }
-  const resDelete = (item) => {
-    setRes(item)
-  }
-  const resReset = () => {
-    setRes('')
-  }
-  const resultHandler = (item) => {
-    setResult(item)
-  }
-  
-  return (
-    <div>
-      <Result res = {res} result = {result} />
-      <Operations
-        res = {res} resHandler = {resHandler}
-        resultHandler = {resultHandler}
-        resReset = {resReset} resDelete = {resDelete}
-      />
-    </div>
-  )
-}
+    const [res, setRes] = useState('');
+    const [result, setResult] = useState('');
+    const resHandler = item => {
+        setRes(res + item);
+    };
+    const resDelete = item => {
+        setRes(item);
+    };
+    const resReset = () => {
+        setRes('');
+    };
+    const resultHandler = item => {
+        setResult(item);
+    };
 
-export default App
+    return (
+        <div>
+            <Result res={res} result={result} />
+            <Operations
+                res={res}
+                resHandler={resHandler}
+                resultHandler={resultHandler}
+                resReset={resReset}
+                resDelete={resDelete}
+            />
+        </div>
+    );
+};
+
+export default App;
